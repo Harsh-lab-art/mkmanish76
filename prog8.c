ea732b320ed29fcc1788d1ab8351fb81f497cc94
Manish Kumar <manish98uuu@gmail.com>
2024-12-31 13:39:10 +0530

    Season month in 1-12

---
+++ prog8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the int");
+    scanf("%d",&a);
+    if( a==11|| a==12|| a==1|| a==2)
+    printf("winter seassion");
+    else if (a==3|| a==4|| a==5||a==6)
+    printf("summer seasson");
+    else if (a==7|| a==8|| a==9|| a==10)
+    printf("Rainy season");
+return 0;
+}
