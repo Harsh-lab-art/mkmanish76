9f89ed646a865e9173a48007e331faa432b13f77
Manish Kumar <manish98uuu@gmail.com>
2025-01-03 00:57:29 +0530

    Tringular no. 1234 pattern.

---
+++ prog21.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//using nested loop ( Tringular 123.. pattern)
+int main()
+{
+    int i,j;
+    for(i=1; i<=4; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
