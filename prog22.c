8272b78153ac8676dd7be2bfaaa1f732d1794117
Manish Kumar <manish98uuu@gmail.com>
2025-01-04 11:21:56 +0530

    Tringular 1,22,333, pattern.

---
+++ prog22.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+// using nested loop( 1,22,333, pattern)
+int main()
+{
+int i,j;
+for(i=1; i<=4; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
