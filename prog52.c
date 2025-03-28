007342633146cde42af1d6d713944b5c30f9e9fd
Manish Kumar <manish98uuu@gmail.com>
2025-03-28 23:49:14 +0530

    Another way of passing Array As Argument

---
+++ prog52.c
@@ -0,0 +1,21 @@
+//Another way of passing Array As Argument
+#include <stdio.h>
+ void display(int []); 
+ int main()
+{
+int arr[5];
+for(int i=0;i<5;i++) 
+{
+    printf("enter no:");
+    scanf("%d",&arr[i]);
+}
+display(arr); 
+return 0;
+}
+ void display(int brr[5])
+ {
+for(int i=0;i<5;i++)
+{
+printf("\n%d",brr[i]);
+}
+}
