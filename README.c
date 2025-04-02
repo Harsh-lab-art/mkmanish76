d6283a4a1f447955ad9c9f4c30323d04b86d9159
Manish Kumar <manish98uuu@gmail.com>
2025-04-02 19:08:48 +0530

    Update README.c

--- README.c
+++ README.c
@@ -1,1 +1,18 @@
+# 💫 About Me:
+🔭 I’m currently working on Helping you craft engaging and creative LinkedIn posts to enhance your professional presence.<br><br><br>👯 I’m looking to collaborate on Writing strategies, content ideas, and storytelling techniques to make your LinkedIn posts stand out.<br>🤝 I’m looking for help with Understanding more about your industry and goals so I can tailor my advice even better.<br>🌱 I’m currently learning More about your style and preferences in writing to make my suggestions more aligned with your needs.<br>💬 Ask me about How to write compelling LinkedIn posts, storytelling techniques, content strategies, or anything related to creative writing.<br>⚡ Fun fact A well-crafted LinkedIn post with a strong hook and personal storytelling can boost engagement by over 200% compared to a generic update!
 
+
+## 🌐 Socials:
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-%230077B5.svg?logo=linkedin&logoColor=white)](https://linkedin.com/in/www.linkedin.com/in/ manish-kumar-426aa8304) [![X](https://img.shields.io/badge/X-black.svg?logo=X&logoColor=white)](https://x.com/: https://x.com/ManishKuma846?t=Hg9BhTM-rETHS2K3IulA8A&s=08 ) [![YouTube](https://img.shields.io/badge/YouTube-%23FF0000.svg?logo=YouTube&logoColor=white)](https://youtube.com/@https://youtube.com/@its_manish_videos?si=aIBdP1vaL45dxIZN) [![email](https://img.shields.io/badge/Email-D14836?logo=gmail&logoColor=white)](mailto:manish98uuu@gmail.com) 
+
+# 💻 Tech Stack:
+![C](https://img.shields.io/badge/c-%2300599C.svg?style=for-the-badge&logo=c&logoColor=white) ![C++](https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white) ![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+# 📊 GitHub Stats:
+![](https://github-readme-stats.vercel.app/api?username=mkmanish76&theme=dark&hide_border=false&include_all_commits=false&count_private=false)<br/>
+![](https://nirzak-streak-stats.vercel.app/?user=mkmanish76&theme=dark&hide_border=false)<br/>
+![](https://github-readme-stats.vercel.app/api/top-langs/?username=mkmanish76&theme=dark&hide_border=false&include_all_commits=false&count_private=false&layout=compact)
+
+---
+[![](https://visitcount.itsvg.in/api?id=mkmanish76&icon=0&color=0)](https://visitcount.itsvg.in)
+
+<!-- Proudly created with GPRM ( https://gprm.itsvg.in ) -->
