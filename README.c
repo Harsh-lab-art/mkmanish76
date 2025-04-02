9136e3c0431acee90ff7bb087c782b6d98c9f3fa
Manish Kumar <manish98uuu@gmail.com>
2025-04-02 18:48:33 +0530

    Update README.c

--- README.c
+++ README.c
@@ -1,13 +1,1 @@
-#include<stdio.h>
 
-int main() {
-   float radius, area;
-
-   printf("\nEnter the radius of Circle : ");
-   scanf("%d", &radius);
-
-   area = 3.14 * radius * radius;
-   printf("\nArea of Circle : %f", area);
-
-   return (0);
-}
